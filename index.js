9401a46fa0fe4ace20070a00b1eaa2d2d99d5242
Elizabeth Haro <elizabeth.haro@ymail.com>
2023-03-26 20:04:47 -0700

    Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+
+const start = 42;
+
+function distanceFromHqInBlocks(blocks){
+    return Math.abs(blocks - start);
+}
+
+function distanceFromHqInFeet(feet){
+    return distanceFromHqInBlocks(feet) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    let feetTraveled = Math.abs(start - destination) * 264;
+    let farePrice;
+    if(feetTraveled <= 400){
+        farePrice = 0
+        return farePrice;
+    } else if (400 < feetTraveled && feetTraveled <= 2000){
+        farePrice = 2.56
+        return farePrice;
+    } else if (2000 < feetTraveled && feetTraveled <= 2500){
+        farePrice = 25
+        return farePrice;
+    } else {
+        return `cannot travel that far`;
+    }
+
+}
